522c76cf2be748446757a19de6a0ba35fe1cf2cf
warnakulasuriya-fds-e23 <warnakulasuriya_fds_e23@engug.ruh.ac.lk>
2025-01-01 11:58:47 +0530

    basic implementation of UDPClient
    also created UPDServer class

---
+++ src/MessageApp/UDPClient.java
@@ -0,0 +1,23 @@
+package MessageApp;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+public class UDPClient {
+	static int port = 4411;
+	public static void main(String[] args) throws IOException {
+		DatagramSocket client = new DatagramSocket();
+		InetAddress ipAddress = InetAddress.getLocalHost();
+		
+		String str = "Hello World";
+		byte[] buf = str.getBytes();
+		
+		DatagramPacket pac = new DatagramPacket(buf, buf.length, ipAddress, port);
+		client.send(pac);
+		
+		client.close();
+		
+	}
+}
---
+++ src/MessageApp/UDPServer.java
@@ -0,0 +1,5 @@
+package MessageApp;
+
+public class UDPServer {
+
+}
