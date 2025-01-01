d721fea3fc72005f0b6ff34d0b0d196e685d9922
warnakulasuriya-fds-e23 <warnakulasuriya_fds_e23@engug.ruh.ac.lk>
2025-01-01 12:13:19 +0530

    basic implementation of UDPServer

--- src/MessageApp/UDPServer.java
+++ src/MessageApp/UDPServer.java
@@ -1,5 +1,23 @@
 package MessageApp;
 
-public class UDPServer {
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 
+public class UDPServer {
+	static int port = 4411;
+	
+	public static void main(String[] args) throws IOException {
+		DatagramSocket server = new DatagramSocket(port);
+		byte[] buf = new byte[256];
+		
+		DatagramPacket packet = new DatagramPacket(buf,buf.length);
+		server.receive(packet);
+		
+		String response = new String(packet.getData());
+		
+		System.out.print("Received Data: " + response);
+		
+		server.close();
+	}
 }
