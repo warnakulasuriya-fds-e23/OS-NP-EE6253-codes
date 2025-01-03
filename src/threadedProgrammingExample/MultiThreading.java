29326a6ab1d9673cd4f3b7f376e8c13489540e52
warnakulasuriya-fds-e23 <warnakulasuriya_fds_e23@engug.ruh.ac.lk>
2025-01-03 09:26:32 +0530

    used multiple threads to perform same task

--- src/threadedProgrammingExample/MultiThreading.java
+++ src/threadedProgrammingExample/MultiThreading.java
@@ -1,10 +1,14 @@
 package threadedProgrammingExample;
 
 public class MultiThreading extends Thread{
+	private int thread_number;
 	
+	public MultiThreading(int t) {
+		this.thread_number = t;
+	}
 	public void run() {
 		for(int i=0; i<=5; i++) {
-			System.out.println(i);
+			System.out.println(i+" is from : "+thread_number);
 			
 			try {
 				Thread.sleep(1000);//sleep for 1000ms
--- src/threadedProgrammingExample/ThreadHandler.java
+++ src/threadedProgrammingExample/ThreadHandler.java
@@ -3,9 +3,13 @@ package threadedProgrammingExample;
 public class ThreadHandler {
 
 	public static void main(String[] args) {
-		MultiThreading t = new MultiThreading();
+		MultiThreading t1 = new MultiThreading(1);
+		MultiThreading t2 = new MultiThreading(2);
+		MultiThreading t3 = new MultiThreading(3);
 		
-		t.start();
+		t1.start();
+		t2.start();
+		t3.start();
 
 	}
 
